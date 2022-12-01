88f803b9cd76b68ddbe6a8a0054f4d9b9f33f9d3
Anton Nocum <anton.nocum@gmail.com>
2022-12-02 07:56:10 +0800

    first commit

---
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -0,0 +1,106 @@
+import {
+  CreateContactMutation,
+  CreateContactMutationVariables,
+} from 'types/graphql'
+
+import {
+  FieldError,
+  Form,
+  FormError,
+  Label,
+  Submit,
+  SubmitHandler,
+  TextAreaField,
+  TextField,
+  useForm,
+} from '@redwoodjs/forms'
+import { MetaTags, useMutation } from '@redwoodjs/web'
+import { toast, Toaster } from '@redwoodjs/web/toast'
+
+interface FormValues {
+  name: string
+  email: string
+  message: string
+}
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
+
+const ContactPage = () => {
+  const [create, { loading, error }] = useMutation<
+    CreateContactMutation,
+    CreateContactMutationVariables
+  >(CREATE_CONTACT, {
+    onCompleted: () => {
+      toast.success('Thank you for your submission!')
+      formMethods.reset()
+    },
+  })
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    create({
+      variables: {
+        input: data,
+      },
+    })
+  }
+  const formMethods = useForm()
+
+  return (
+    <>
+      <MetaTags title="Contact" description="Contact page" />
+
+      <Toaster />
+      <Form
+        onSubmit={onSubmit}
+        config={{ mode: 'onBlur' }}
+        formMethods={formMethods}
+      >
+        <FormError error={error} wrapperClassName="form-error" />
+        <Label name="name" errorClassName="error">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{
+            required: true,
+          }}
+          errorClassName="error"
+        />
+        <FieldError className="error" name="name" />
+
+        <Label name="email" errorClassName="error">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+          }}
+          errorClassName="error"
+        />
+        <FieldError className="error" name="email" />
+
+        <Label name="message" errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{
+            required: true,
+          }}
+          errorClassName="error"
+        />
+        <FieldError className="error" name="message" />
+
+        <Submit disabled={loading}>Save</Submit>
+      </Form>
+    </>
+  )
+}
+
+export default ContactPage
